368fe98758dad9e152198e2cfb81bbee9b7b4996
li.chen <clxhhsy@126.com>
2016-12-16 17:23:36 +0800

    first commit

--- src/android/com/forbetter/plugins/baidupush/BaidupushPlugin.java
+++ src/android/com/forbetter/plugins/baidupush/BaidupushPlugin.java
@@ -14,6 +14,7 @@ import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * 百度推送插件类
@@ -27,7 +28,7 @@ public class BaidupushPlugin extends CordovaPlugin {
 
     private static final String LOG_TAG = "BaiduPushPlugin";
 
-    private ExecutorService threadPool = cordova.getThreadPool();
+    private ExecutorService threadPool = Executors.newFixedThreadPool(1);
 
     /* JS回调上下文接口 */
     static CallbackContext StartWorkCallbackContext = null;
