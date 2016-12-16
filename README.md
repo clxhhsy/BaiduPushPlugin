51504fc67623b5690fa33500dec4bd860d6a36f9
li.chen <clxhhsy@126.com>
2016-12-16 16:43:32 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+#cordova-plugin-baidupushplugin
+百度推送cordova插件
+目前仅支持android
+后续支持ios
+
+#安装
+cordova plugin add https://github.com/clxhhsy/BaiduPushPlugin.git
+
+@see https://github.com/EtherZhou/baidupush.git
--- www/BaiduPushPlugin.js
+++ www/BaiduPushPlugin.js
@@ -11,6 +11,21 @@ BaiduPush.prototype.startWork = function (apiKey, successCallback) {
     console.log('baidupush startWork with apikey : ' + apiKey);
     exec(successCallback, failureCallback, 'BaiduPush', 'startWork', [apiKey]);
 };
+BaiduPush.prototype.stopWork = function (successCallback) {
+    exec(successCallback, failureCallback, 'BaiduPush', 'stopWork', []);
+};
+BaiduPush.prototype.resumeWork = function (successCallback) {
+    exec(successCallback, failureCallback, 'BaiduPush', 'resumeWork', []);
+};
+BaiduPush.prototype.listenNotificationClicked = function ( successCallback) {
+    exec(successCallback, failureCallback, 'BaiduPush', 'listenNotificationClicked', []);
+};
+BaiduPush.prototype.listenMessage = function ( successCallback) {
+    exec(successCallback, failureCallback, 'BaiduPush', 'listenMessage', []);
+};
+BaiduPush.prototype.listenNotificationArrived = function ( successCallback) {
+    exec(successCallback, failureCallback, 'BaiduPush', 'listenNotificationArrived', []);
+};
 
 var baiduPush = new BaiduPush();
 module.exports = baiduPush;
